c3f0c73857143fda50260669d365fc1356e8bd4b
UbaidRaza-1312 <razau3136@gmail.com>
2026-02-01 10:47:29 +0500

    Updated FloatingChatButton component

--- frontend/src/components/FloatingChatButton.tsx
+++ frontend/src/components/FloatingChatButton.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect, useRef } from 'react';
 import { useAuth } from '../contexts/AuthContext';
-import { apiClient } from '../../lib/api-client';
+import { apiClient } from '../lib/api-client';
 
 interface Message {
   id: string;
@@ -123,7 +123,7 @@ const FloatingChatButton = () => {
         aria-label="Open chat"
       >
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574[...]" />
         </svg>
       </button>
 
@@ -138,7 +138,7 @@ const FloatingChatButton = () => {
               className="text-gray-400 hover:text-white"
             >
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414[...]" />
               </svg>
             </button>
           </div>
@@ -162,7 +162,7 @@ const FloatingChatButton = () => {
                     msg.role === 'user'
                       ? 'text-blue-200'
                       : 'text-gray-400'
-                  }`}>
+                  }`}> 
                     {formatTime(msg.created_at)}
                   </div>
                 </div>
